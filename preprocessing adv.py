9b3a5b6a74a826c2d102d6f30a7f4d10bde8d362
Manos Gaitanakis <manosgaitanakis93@gmail.com>
2019-03-26 21:35:42 +0000

    Add files via upload

--- preprocessing adv.py
+++ preprocessing adv.py
@@ -3,14 +3,12 @@
 Created on Mon Mar 18 09:14:07 2019
 @author: fro
 """
-
-# import the necessary packages
 import numpy as np
 import cv2
 import imutils
 import matplotlib.pyplot as plt
 # load the image, convert it to grayscale, and blur it to remove noise
-image = cv2.imread("sample1.jpg")
+image = cv2.imread("sample5.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (7, 7), 0)
 # threshold the image
@@ -18,21 +16,24 @@ ret,thresh= cv2.threshold(gray ,127,255,cv2.THRESH_BINARY_INV)
 
 # dilate the white portions
 dilate = cv2.dilate(thresh, None, iterations=2)
+def segmentation(arr):
+    list = []
+    for i in range(len(arr)):
+        if sum(arr[i])== 0:
+            list.append(i)
+        else:
+            list.append(0)   
+    nlist=[]
 
-list = []
-for i in range(len(dilate)):
-    if sum(dilate[i])== 0:
-        list.append(i)
-    else:
-        list.append(0)
-nlist=[]
-try:
-    if list[0] < list[1]:
-        for i in range(1,len(list)):
-            if list[i] > list[i+1] or list[i] == 0 and list[i+1] > list[i] :
-                nlist.append(i)
-except IndexError:
-    pass  
+    try:
+        if list[0] < list[1]:
+            for i in range(1,len(list)):
+                if list[i] > list[i+1] or list[i] == 0 and list[i+1] > list[i] :
+                    nlist.append(i)
+    except IndexError:
+        pass  
+    return list,nlist      
+list,nlist = segmentation(dilate)
 list2 = []
 suma = 0
 row = 1
@@ -50,7 +51,7 @@ for i in range (0,len(nlist),2):
         suma = 0
     row+=1 
     list2 =[]   
-    
+  
 nlist2=[]
 letterpos = {}
 
@@ -66,15 +67,165 @@ for y in range(1,len(rows)+1):
                     letterpos[y] = nlist2
             except IndexError:
                 pass   
-                    
-    nlist2 = []
+    nlist2 = [] 
+    '''
+pos=1   
+for i in range (0,len(nlist),2):
+    try:
+        for z in range (0,len(letterpos[pos]),2):
+           rowtop,rowbottom = nlist[i],nlist[i+1]
+           letterfront,letterend= letterpos[pos][i],letterpos[pos][i+1]
+           new = np.array(dilate[rowtop:rowbottom,letterfront:letterend])              
+    except IndexError:
+                pass   
 
+'''
+dilate2= dilate
+image = cv2.cvtColor(dilate2,cv2.COLOR_GRAY2BGR)
+#dilate2 = cv2.rectangle(dilate2,pt1=(100,100),pt2=(100,100),color=(0,255,0),thickness=100)
+all_letters = np.zeros(shape=(1,28,28))
+pos=0  
+row=1  
+notempty=[]
+notemptySize=0
+inc=0
+if len(nlist) < 3 and len(letterpos)==1 :
+        for i in range (0,len(letterpos[1]),2):
+            print(i)
+            try:
+                for z in range (0,len(letterpos),2):
+                   print("z = ",z)
+                   rowtop,rowbottom = nlist[0],nlist[1]
+                   letterfront,letterend= letterpos[row][inc],letterpos[row][inc+1]
+                   new = np.array(dilate[rowtop:rowbottom,letterfront:letterend])
+                   noUse,trimmed = segmentation(new)
+                   notempty.append(trimmed)
+                   #if len(notempty[notemptySize]) == 0:
+                       #print('yes')
+                   cv2.rectangle(image,pt1=(letterpos[1][pos],nlist[0]),pt2=(letterpos[1][pos+1],nlist[1]),color=(0,255,0),thickness=2)
+                   #else:
+                       #print('no')
+                       #new = new[trimmed[0]:trimmed[1],:]
+                       #cv2.rectangle(image,pt1=(letterpos[1][pos],nlist[0]-300),pt2=(letterpos[1][pos+1],nlist[1]-abs(notempty[inc][1] - len(noUse))),color=(0,255,0),thickness=2)
+                   new = np.pad(new, (5, 5), 'minimum')
+                   resized_image = cv2.resize(new, (28, 28))
+                   resized_image2 = np.expand_dims(resized_image, axis=0)
+                   all_letters = np.append(all_letters,resized_image2,axis=0)
+                   notemptySize+=1
+            except IndexError:
+                            pass           
+            pos+=2
+            inc+=1
+            if len(letterpos)> 1:
+                row=1 
+        all_letters = np.delete(all_letters,[0],axis=0)
+else:
+        for i in range (0,len(letterpos),2):
+            print(i)
+            try:
+                for z in range (0,len(letterpos[i]),2):
+                   print("z = ",z)
+                   rowtop,rowbottom = nlist[0],nlist[1]
+                   letterfront,letterend= letterpos[row][inc],letterpos[row][inc+1]
+                   new = np.array(dilate[rowtop:rowbottom,letterfront:letterend])
+                   noUse,trimmed = segmentation(new)
+                   notempty.append(trimmed)
+                   #if len(notempty[notemptySize]) == 0:
+                       #print('yes')
+                   cv2.rectangle(image,pt1=(letterpos[1][pos],nlist[0]),pt2=(letterpos[1][pos+1],nlist[1]),color=(0,255,0),thickness=2)
+                   #else:
+                       #print('no')
+                       #new = new[trimmed[0]:trimmed[1],:]
+                       #cv2.rectangle(image,pt1=(letterpos[1][pos],nlist[0]-300),pt2=(letterpos[1][pos+1],nlist[1]-abs(notempty[inc][1] - len(noUse))),color=(0,255,0),thickness=2)
+                   new = np.pad(new, (5, 5), 'minimum')
+                   resized_image = cv2.resize(new, (28, 28))
+                   resized_image2 = np.expand_dims(resized_image, axis=0)
+                   all_letters = np.append(all_letters,resized_image2,axis=0)
+                   notemptySize+=1
+            except IndexError:
+                            pass           
+            pos+=2
+            inc+=1
+            if len(letterpos)> 1:
+                row=1 
+        all_letters = np.delete(all_letters,[0],axis=0)    
+plt.imshow(image)
+#-notempty[1][0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if len(nlist) == 2:
+    for i in range (0,len(letterpos[1]),2):
+        print(i)
+        
+    
+cv2.rectangle(image,pt1=(letterpos[1][2]-notempty[1][0],nlist[0]),pt2=(letterpos[1][3],nlist[1]-notempty[1][1]),color=(0,255,0),thickness=2)
+#dilate2 = np.resize(dilate2, (dilate2[0].size, dilate2[1].size, 3))
+
+if notemptySize[0]
+'''else:
+    pos=1    
+    for i in range (0,len(nlist),2):
+        try:
+            for z in range (0,len(letterpos[pos]),2):
+               rowtop,rowbottom = nlist[i],nlist[i+1]
+               letterfront,letterend= letterpos[pos][i],letterpos[pos][i+1]
+               noUse,trimmed = segmentation(new)
+               new = np.array(dilate[rowtop:rowbottom,letterfront:letterend])
+               new= new[trimmed[0]:trimmed[1],:]
+               new = np.pad(new, (5, 5), 'minimum')
+               resized_image = cv2.resize(new, (28, 28))
+               resized_image2 = np.expand_dims(resized_image, axis=0)
+               all_letters = np.append(all_letters,resized_image2,axis=0)
+        except IndexError:
+                        pass           
+        pos+=1 
+    all_letters = np.delete(all_letters,[0],axis=0)
+
+ '''              
+charachter_trimmed = new[trimmed[0]:trimmed[1],:]
+               
+               
+rowtop,rowbottom = nlist[0],nlist[0+1]
+letterfront,letterend= letterpos[1][2],letterpos[0][0+1]
+new = np.array(dilate[rowtop:rowbottom,letterfront:letterend])
+
+rowtop,rowbottom = 6,156
+letterfront,letterend= 251,278
+new = np.array(dilate[rowtop:rowbottom,letterfront:letterend])
+
+plt.imshow(new[7:33,:])
+newtop,newbot = segmentation(new)
 
 
 new = np.array(dilate[nlist[2]:nlist[2+1],:])
-new = np.array(dilate[22:134,136:222])
+new = np.array(dilate[69:157,162:219])
+plt.imshow(all_letters[1])
 plt.imshow(new)
-
 
 # find contours in the image
 cnts = cv2.findContours(dilate.copy(), cv2.RETR_EXTERNAL,
