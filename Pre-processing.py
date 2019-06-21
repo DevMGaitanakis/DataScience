615cb7494e524c1260a3d581a3cc1fdb0579b294
Manos Gaitanakis <manosgaitanakis93@gmail.com>
2019-06-21 16:52:52 +0100

    Update Pre-processing.py

--- Pre-processing.py
+++ Pre-processing.py
@@ -4,12 +4,19 @@ Created on Mon Jun  3 12:49:36 2019
 
 @author: fro
 """
-
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import itertools
+import math
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 %matplotlib qt
 data = pd.read_csv('kilauea.csv').to_numpy(dtype='float32')
 dataset =np.flipud(data)
@@ -113,7 +120,7 @@ def dataoversample(cutstart,cutend,dataset):
             if dataset_to_repair[i,1] != month:
                 #print('not equal month')
                 gap = abs(days - month_days)+1
-                print('Gap is',gap)
+                #print('Gap is',gap)
                 for c in range(gap):
                     if c == 0:
                         mean_flunct =  (dataset_to_repair[i,3] +  dataset_to_repair[i-1,3])/2
@@ -182,29 +189,29 @@ dataset_repaired = dataoversample(2013,2018,new_dataset)
 for i in range(len(dataset_repaired)):
     dataset_repaired[i] = dataset_repaired[i].flatten() #Flatten arrays saved in the list
 
-new_dataset = pd.DataFrame(dataset_repaired)
-new_dataset.to_csv("Oversampledkilanuea.csv") #Export to CSV
+#new_dataset = pd.DataFrame(dataset_repaired)
+#new_dataset.to_csv("Oversampledkilanuea.csv") #Export to CSV
 
+dataset_repaired = np.array(dataset_repaired,np.object)
+for i in range(len(dataset_repaired)):
+    dataset_repaired[i,0] = str(int(dataset_repaired[i,0])) +'-'+ str(int(dataset_repaired[i,1]))+'-'+str(int(dataset_repaired[i,2]))
 
-for i in range(len(new_dataset)):
-    new_dataset[i,0] = str(int(new_dataset[i,0])) +'-'+ str(int(new_dataset[i,1]))+'-'+str(int(new_dataset[i,2]))
+dataset_repaired = np.delete(dataset_repaired, 1, 1) 
+dataset_repaired = np.delete(dataset_repaired, 1, 1) 
+dataset_repaired = np.delete(dataset_repaired, 2, 1) 
+dataset_repaired = pd.DataFrame(dataset_repaired)
 
-new_dataset = np.delete(new_dataset, 1, 1) 
-new_dataset = np.delete(new_dataset, 1, 1) 
-new_dataset = np.delete(new_dataset, 2, 1) 
-new_dataset = pd.DataFrame(new_dataset)
+dataset_repaired[0] = pd.to_datetime(dataset_repaired[0]) # Covenrt to Date Time
+dataset_repaired[1] = dataset_repaired[1].astype(float) # Conver from object to float
+dataset_repaired.dtypes
 
-new_dataset[0] = pd.to_datetime(new_dataset[0]) # Covenrt to Date Time
-new_dataset[1] = new_dataset[1].astype(float) # Conver from object to float
-new_dataset.dtypes
+dataset_repaired = dataset_repaired.set_index(0)
 
-new_dataset = new_dataset.set_index(0)
+#sns.set(rc={'figure.figsize':(11, 4)})
+#dataset_repaired[1].plot(linewidth=0.5); #Plotting the Data
 
-sns.set(rc={'figure.figsize':(11, 4)})
-new_dataset[1].plot(linewidth=0.5); #Plotting the Data
-
-to_train= new_dataset.loc[:'2017-07-12']
-to_test= new_dataset.loc['2018-08-07':]
+to_train= dataset_repaired.loc[:,:]
+#to_test= new_dataset.loc['2018-08-07':]
 
 #####Prediction model Starts Here#####
 
@@ -224,31 +231,259 @@ def split_sequence(sequence,n_steps):
     return np.array(X), np.array(y)
 
 sequence = np.array(to_train)
-from sklearn import preprocessing
-sequence1 =preprocessing.scale(sequence)
-n_steps = 7
-X,y =  split_sequence(sequence,n_steps)
+scaler = MinMaxScaler(feature_range=(0,1))
+sequence = scaler.fit_transform(sequence)
+'''
+#Splitting into training and test set
+training_seq = int(len(sequence)*0.80)
+test_seq = len(sequence) - training_seq
+train,test = sequence[0:training_seq,:],sequence[training_seq:len(sequence),:]
 
+#Transforming the dataset into timesteps selected
+n_steps = 14
+trainX,trainy =  split_sequence(train,n_steps)
+testX,testy = split_sequence(test,n_steps)
+
+#Transforming into the format expected from the LSTM
 n_features = 1
-X = X.reshape((X.shape[0],X.shape[1],n_features))
+trainX = trainX.reshape((trainX.shape[0],trainX.shape[1],n_features))
+testX = testX.reshape((testX.shape[0],testX.shape[1],n_features))
 
+
+#Build and train the neural network
 model = Sequential()
-model.add(LSTM(1, return_sequences=True,
-               input_shape=(n_steps, 1)))  # returns a sequence of vectors of dimension 32
-model.add(LSTM(100, return_sequences=True)) 
-model.add(LSTM(100, return_sequences=True))
-model.add(LSTM(100, return_sequences=True)) # returns a sequence of vectors of dimension 32
-model.add(LSTM(100))  # return a single vector of dimension 32
+model.add(LSTM(20, return_sequences=True,input_shape=(n_steps, 1)))  # returns a sequence of vectors of dimension 32
+model.add(LSTM(20,input_shape=(n_steps, 1)))  # returns a sequence of vectors of dimension 32
 model.add(Dense(1))
 model.compile(optimizer='adam',loss='mse')
-model.fit(X,y,epochs=50)
+model.fit(trainX,trainy,epochs=10,shuffle = False)
 
-x_predict = np.array(to_test)
-x_predict= x_predict[0:7]
+#Make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
 
-x_predict = x_predict.reshape(1,n_steps,n_features)
-yhat = model.predict(x_predict)
-print(yhat)
+#Invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainy = scaler.inverse_transform(trainy)
+
+testPredict = scaler.inverse_transform(testPredict)
+testy = scaler.inverse_transform(testy)
+
+#Calculate RMSE
+trainScore = math.sqrt(mean_squared_error(trainy,trainPredict))
+print('Train score : %.2f RMSE' %(trainScore))
+testScore = math.sqrt(mean_squared_error(testy,testPredict))
+print('Train score : %.2f RMSE' %(testScore))
+
+
+trainPredictPlot = np.empty_like(dataset_repaired)
+trainPredictPlot[:,:] = np.nan
+trainPredictPlot[n_steps:len(trainPredict)+n_steps,:] = trainPredict
+
+testPredictPlot = np.empty_like(dataset_repaired)
+testPredictPlot[:,:] = np.nan
+testPredictPlot[len(trainPredict)+(n_steps*2)+1:len(dataset_repaired)-1,:] = testPredict
+
+'''
 
 
 
+
+# ----------------------------------------------------------
+# EDITABLE PARAMETERS
+# Read the documentation in the script head for more details
+# ----------------------------------------------------------
+
+# length of input
+input_len = len(sequence)
+
+# The window length of the moving average used to generate
+# the output from the input in the input/output pair used
+# to train the LSTM
+# e.g. if tsteps=2 and input=[1, 2, 3, 4, 5],
+#      then output=[1.5, 2.5, 3.5, 4.5]
+tsteps = 2
+
+# The input sequence length that the LSTM is trained on for each output point
+lahead = 14
+
+# training parameters passed to "model.fit(...)"
+batch_size = 1
+epochs = 50
+
+# ------------
+# MAIN PROGRAM
+# ------------
+
+print("*" * 33)
+if lahead >= tsteps:
+    print("STATELESS LSTM WILL ALSO CONVERGE")
+else:
+    print("STATELESS LSTM WILL NOT CONVERGE")
+print("*" * 33)
+'''
+np.random.seed(1986)
+
+print('Generating Data...')
+
+
+def gen_uniform_amp(amp=1, xn=10000):
+    """Generates uniform random data between
+    -amp and +amp
+    and of length xn
+
+    Arguments:
+        amp: maximum/minimum range of uniform data
+        xn: length of series
+    """
+    data_input = np.random.uniform(-1 * amp, +1 * amp, xn)
+    data_input = pd.DataFrame(data_input)
+    return data_input
+'''
+# Since the output is a moving average of the input,
+# the first few points of output will be NaN
+# and will be dropped from the generated data
+# before training the LSTM.
+# Also, when lahead > 1,
+# the preprocessing step later of "rolling window view"
+# will also cause some points to be lost.
+# For aesthetic reasons,
+# in order to maintain generated data length = input_len after pre-processing,
+# add a few points to account for the values that will be lost.
+to_drop = max(tsteps - 1, lahead - 1)
+data_input = dataset_repaired
+
+# set the target to be a N-point average of the input
+expected_output = data_input.rolling(window=tsteps, center=False).mean()
+
+# when lahead > 1, need to convert the input to "rolling window view"
+# https://docs.scipy.org/doc/numpy/reference/generated/numpy.repeat.html
+if lahead > 1:
+    data_input = np.repeat(data_input.values, repeats=lahead, axis=1)
+    data_input = pd.DataFrame(data_input)
+    for i, c in enumerate(data_input.columns):
+        data_input[c] = data_input[c].shift(i)
+
+# drop the nan
+expected_output = expected_output[to_drop:]
+data_input = data_input[to_drop:]
+
+print('Input shape:', data_input.shape)
+print('Output shape:', expected_output.shape)
+print('Input head: ')
+print(data_input.head())
+print('Output head: ')
+print(expected_output.head())
+print('Input tail: ')
+print(data_input.tail())
+print('Output tail: ')
+print(expected_output.tail())
+
+print('Plotting input and expected output')
+plt.plot(data_input[0][:10], '.')
+plt.plot(expected_output[0][:10], '-')
+plt.legend(['Input', 'Expected output'])
+plt.title('Input')
+plt.show()
+
+
+def create_model(stateful):
+    model = Sequential()
+    model.add(LSTM(20,
+              input_shape=(lahead, 1),
+              batch_size=batch_size,
+              stateful=stateful))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    return model
+
+print('Creating Stateful Model...')
+model_stateful = create_model(stateful=True)
+
+
+# split train/test data
+def split_data(x, y, ratio=0.8):
+    to_train = int(input_len * ratio)
+    # tweak to match with batch_size
+    to_train -= to_train % batch_size
+
+    x_train = x[:to_train]
+    y_train = y[:to_train]
+    x_test = x[to_train:]
+    y_test = y[to_train:]
+
+    # tweak to match with batch_size
+    to_drop = x.shape[0] % batch_size
+    if to_drop > 0:
+        x_test = x_test[:-1 * to_drop]
+        y_test = y_test[:-1 * to_drop]
+
+    # some reshaping
+    reshape_3 = lambda x: x.values.reshape((x.shape[0], x.shape[1], 1))
+    x_train = reshape_3(x_train)
+    x_test = reshape_3(x_test)
+
+    reshape_2 = lambda x: x.values.reshape((x.shape[0], 1))
+    y_train = reshape_2(y_train)
+    y_test = reshape_2(y_test)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+(x_train, y_train), (x_test, y_test) = split_data(data_input, expected_output)
+print('x_train.shape: ', x_train.shape)
+print('y_train.shape: ', y_train.shape)
+print('x_test.shape: ', x_test.shape)
+print('y_test.shape: ', y_test.shape)
+
+print('Training')
+for i in range(epochs):
+    print('Epoch', i + 1, '/', epochs)
+    # Note that the last state for sample i in a batch will
+    # be used as initial state for sample i in the next batch.
+    # Thus we are simultaneously training on batch_size series with
+    # lower resolution than the original series contained in data_input.
+    # Each of these series are offset by one step and can be
+    # extracted with data_input[i::batch_size].
+    model_stateful.fit(x_train,
+                       y_train,
+                       batch_size=batch_size,
+                       epochs=1,
+                       verbose=1,
+                       validation_data=(x_test, y_test),
+                       shuffle=False)
+    model_stateful.reset_states()
+
+print('Predicting')
+predicted_stateful = model_stateful.predict(x_test, batch_size=batch_size)
+
+print('Creating Stateless Model...')
+model_stateless = create_model(stateful=False)
+
+print('Training')
+model_stateless.fit(x_train,
+                    y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test),
+                    shuffle=False)
+
+print('Predicting')
+predicted_stateless = model_stateless.predict(x_test, batch_size=batch_size)
+
+# ----------------------------
+
+print('Plotting Results')
+plt.subplot(3, 1, 1)
+plt.plot(y_test)
+plt.title('Expected')
+plt.subplot(3, 1, 2)
+# drop the first "tsteps-1" because it is not possible to predict them
+# since the "previous" timesteps to use do not exist
+plt.plot((y_test - predicted_stateful).flatten()[tsteps - 1:])
+plt.title('Stateful: Expected - Predicted')
+plt.subplot(3, 1, 3)
+plt.plot((y_test - predicted_stateless).flatten())
+plt.title('Stateless: Expected - Predicted')
+plt.show()
